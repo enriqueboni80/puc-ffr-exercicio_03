cc132f646ec60f4d126d0148b8fb01781b67cdb5
rickboni80 <enriqueboni80@gmail.com>
2020-03-03 14:47:11 -0300

    Inserindo Favoritos

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import MoviesServices from "../services/MoviesService"
 import { Link } from "react-router-dom"
 import './movie-list.css'
+import { IoIosStarOutline, IoIosStar } from 'react-icons/io'
 
 
 const MoviesList = () => {
@@ -19,23 +20,44 @@ const MoviesList = () => {
     requestMovies();
   }, [])
 
-  const AdcionarFavorito = (movie) => {
+  
+  const toogleFavorito = (contador, ligado=false) => {
+      if(ligado) {
+        document.getElementsByClassName('add-favorite')[contador].style.display = "block";
+        document.getElementsByClassName('remove-favorite')[contador].style.display = "none";
+      }else{
+        document.getElementsByClassName('add-favorite')[contador].style.display = "none";
+        document.getElementsByClassName('remove-favorite')[contador].style.display = "block";
+      }
+  }
+  
+  
+  const adcionarFavorito = (movie, contador, e) => {
+    e.preventDefault()
+    toogleFavorito(contador)
     dispatch({
       type: 'ADD_FAVORITES',
       movie
     })
   }
 
+  const removerFavorito = (movie, contador, e) => {
+    e.preventDefault()
+    toogleFavorito(contador, true)
+    alert('Apagar Favorito')
+  }
+
   return (
     <div className="container">
       <div className="lista-filmes">
-        {movies.data.results.map(movie => {
+        {movies.data.results.map((movie, contador) => {
           return (
             <article className="filme">
               <img src={`${moviesLocate}/${movie.poster_path}`} alt="capa" />
               <div className="link">
                 <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
-                <button onClick={() => AdcionarFavorito(movie)}> aqui </button>
+                  <a href="/" class="add-favorite" onClick={(e) => adcionarFavorito(movie, contador, e)}> <IoIosStarOutline size={35} /> </a>
+                  <a href="/" style={{display: "none"}} class="remove-favorite" onClick={(e) => removerFavorito(movie, contador, e)}> <IoIosStar size={35} /> </a>
               </div>
             </article>
           )
